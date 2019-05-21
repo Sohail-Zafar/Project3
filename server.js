f240a28de6bf4d988b217d520d5b8d754b8308bf
MSI\jeff <hatcher28@me.com>
2019-05-20 19:32:41 -0700

    prepare for deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // test script for postman
-app.get('/', (req, res) => res.send('API Running'));
+//app.get('/', (req, res) => res.send('API Running'));
 
 // define routes
 app.use('/api/users', require('./routes/api/user'));
@@ -19,5 +19,13 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(PORT, () => console.log(`Server satrted on port ${PORT}`));
