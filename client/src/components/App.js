983c9bd937f214f0f2864cabba704c07d08139b7
MSI\jeff <hatcher28@me.com>
2019-06-02 17:09:18 -0700

    combinding code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 //import React from 'react';
 import React, { useEffect } from 'react';
 import VideoPage from '../components/pages/VideoPage';
@@ -7,6 +7,11 @@ import Landing from '../components/layout/Landing';
 import Register from '../components/auth/Register';
 import Login from '../components/auth/Login';
 import Alert from '../components/layout/Alert';
+import Profile from "./pages/Profile";
+import MyArt from "../components/pages/MyArt";
+import ArtTips from "../components/pages/ArtTips";
+import Footer from '../components/Footer';
+//import Wrapper from "./components/Wrapper";
 // redux 
 import { Provider } from 'react-redux';
 import store from '../store';
@@ -19,33 +24,38 @@ if (localStorage.token) {
 }
 
 const App = () => {
-  useEffect (() => {
+  useEffect(() => {
     store.dispatch(loadUser());
   }, [])
 
   return (
     <Provider store={store}>
 
-   
+
       <Router>
-      
+        <div className="container">
           <Navbar />
-          <Landing />
-          
-          <section className='ui container'>
-            <Alert />
-            <Switch>
-               {/* pages go here */}
-              <Route exact path='/Video' component={VideoPage} />
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              
-            </Switch>
-          </section>
-       
+        </div>
+        <section className='container'>
+
+          <Alert />
+          <Switch>
+            {/* pages go here */}
+            <Route exact path='/Video' component={VideoPage} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/myart" component={MyArt} />
+            <Route exact path="/arttips" component={ArtTips} />
+            <Route exact path="/landing" component={Landing} />
+
+          </Switch>
+        </section>
+        <Footer />
       </Router>
+      
     </Provider>
-  ) 
+  )
 }
 
 export default App;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,18 +3,24 @@ import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
+import logo from "../../img/aa_logo_blu-02.png";
+import "../styles/NavBar.css";
+import "../styles/Wrapper.css";
 
 
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
+    <div className="container">
     <div className="ui secondary  menu">
-      <Link className="active item" to="/">Home</Link>,
+      <Link className="nav-item" to="/landing">Home</Link>
     {/* change names to correct component  */}
-      <Link className="item" to="/damages">Damages</Link>,
-      <Link className="item" to="/drivers">Drivers</Link>,
+      <Link className="nav-item" to="/Profile">Profile</Link>
+      <Link className="nav-item" to="/myart">My Art</Link>
+      <Link className="nav-item" to="/video">Art Tips</Link>
       <div className="right menu">
       <Link onClick={logout}  className="ui item" to="/">Logout</Link>
+    </div>
     </div>
     </div>
     
@@ -22,7 +28,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
   const guestLinks = (
     <div className="right menu">
-      <Link className="ui item" to="/login">Login</Link>,
+      <Link className="ui item" to="/login">Login</Link>
       <Link className="ui item" to="/register">Register</Link>
     </div>
   );
@@ -31,6 +37,17 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   return (
     <div className="ui secondary  menu">
       <div className="right menu">
+      <div className="container">
+      <img
+            className="logoImg"
+            src={logo}
+            alt="Logo"
+            style={{ height: "50px" }}
+          />
+          <p className="aa" style={{ height: "40px" }}>
+            <span>A</span>rtists <span>A</span>ccumulated
+          </p>
+    </div>
         {!loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
       </div>
     </div>
